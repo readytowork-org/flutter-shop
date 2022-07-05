610416456d1955ea8ddd52ff81aa93a743b55209
bantawa04 <rahunn3@gmail.com>
2022-07-05 10:29:42 +0545

    8.16 Using consumer to listen to changes

--- lib/widget/product_item.dart
+++ lib/widget/product_item.dart
@@ -4,29 +4,62 @@ import '../models/product.dart';
 import '../screens/product_detail.dart';
 
 class ProductItem extends StatelessWidget {
-  // final String? id;
-  // final String? title;
-  // final String? description;
-  // final String? imageUrl;
-
-  // const ProductItem(this.id, this.title, this.description, this.imageUrl,
-  //     {Key? key})
-  //     : super(key: key);
-
+  //Method 1: Using provider, this method rebuilds whole widget if Product changes
   @override
+  // Widget build(BuildContext context) {
+  //   final product = Provider.of<Product>(context);
+  //   return ClipRRect(
+  //     borderRadius: BorderRadius.circular(10),
+  //     child: GridTile(
+  //       footer: GridTileBar(
+  //         title: Text(product.title!),
+  //         leading: IconButton(
+  //           icon: Icon(
+  //             product.isFavourite ? Icons.favorite : Icons.favorite_border,
+  //           ),
+  //           onPressed: () => product.toggleFavouriteStatus(),
+  //           color: Theme.of(context).colorScheme.secondary,
+  //         ),
+  //         backgroundColor: Colors.black87,
+  //         trailing: IconButton(
+  //           icon: const Icon(Icons.shopping_cart),
+  //           onPressed: () {},
+  //           color: Theme.of(context).colorScheme.secondary,
+  //         ),
+  //       ),
+  //       child: GestureDetector(
+  //         onTap: () {
+  //           Navigator.of(context).pushNamed(
+  //             ProductDetail.routeName,
+  //             arguments: product.id,
+  //           );
+  //         },
+  //         child: Image.network(
+  //           product.imageUrl!,
+  //           fit: BoxFit.cover,
+  //         ),
+  //       ),
+  //     ),
+  //   );
+  // }
+
+  //Method 2: Using consumer to rebuild some parts of widget
+  //Only icon will rebuild when product changes
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context);
+    final product = Provider.of<Product>(context, listen: false);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
         footer: GridTileBar(
           title: Text(product.title!),
-          leading: IconButton(
-            icon: Icon(
-              product.isFavourite ? Icons.favorite : Icons.favorite_border,
+          leading: Consumer<Product>(
+            builder: (context, product, child) => IconButton(
+              icon: Icon(
+                product.isFavourite ? Icons.favorite : Icons.favorite_border,
+              ),
+              onPressed: () => product.toggleFavouriteStatus(),
+              color: Theme.of(context).colorScheme.secondary,
             ),
-            onPressed: () => product.toggleFavouriteStatus(),
-            color: Theme.of(context).colorScheme.secondary,
           ),
           backgroundColor: Colors.black87,
           trailing: IconButton(
