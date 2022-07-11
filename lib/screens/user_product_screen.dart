0879f42734f54b1fb7ea974b79b479c20d48132d
bantawa04 <rahunn3@gmail.com>
2022-07-11 19:03:58 +0545

    10.15 refresh/reload on pull down

--- lib/screens/user_product_screen.dart
+++ lib/screens/user_product_screen.dart
@@ -8,12 +8,17 @@ import '../widget/drawer.dart';
 class UserProductScreen extends StatelessWidget {
   const UserProductScreen({Key? key}) : super(key: key);
   static const routeName = "/user-product";
+
+  Future<void> _refreshOnPull(BuildContext context) async{
+    await Provider.of<ProductsProvider>(context, listen: false).fetchAndSetProducts();
+    // print('refresh on pull down');
+  }
   @override
   Widget build(BuildContext context) {
     final productData = Provider.of<ProductsProvider>(context);
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Products'),
+        title: const Text('Your Products'),
         actions: <Widget>[
           IconButton(
             onPressed: () {
@@ -24,18 +29,21 @@ class UserProductScreen extends StatelessWidget {
         ],
       ),
       drawer: const SideDrawer(),
-      body: ListView.builder(
-        itemBuilder: (ctx, i) => Column(
-          children: <Widget>[
-            UserProductTile(
-              id: productData.items[i].id,
-              title: productData.items[i].title,
-              imageUrl: productData.items[i].imageUrl,
-            ),
-            const Divider(),
-          ],
+      body: RefreshIndicator(
+        onRefresh: () => _refreshOnPull(context),
+        child: ListView.builder(
+          itemBuilder: (ctx, i) => Column(
+            children: <Widget>[
+              UserProductTile(
+                id: productData.items[i].id,
+                title: productData.items[i].title,
+                imageUrl: productData.items[i].imageUrl,
+              ),
+              const Divider(),
+            ],
+          ),
+          itemCount: productData.items.length,
         ),
-        itemCount: productData.items.length,
       ),
     );
   }
