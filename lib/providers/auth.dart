31d072a80628d6d479413beada3e0c9d78bb0a44
bantawa04 <rahunn3@gmail.com>
2022-07-18 13:33:04 +0545

    11.06 login with firebase

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import "package:http/http.dart" as http;
@@ -8,21 +7,33 @@ class Auth with ChangeNotifier {
   String? token;
   DateTime? time;
   String? userId;
+  static const String _appKey = "AIzaSyAAisBCWejKaemIPKk1AGZca-PaB3pxhZw";
 
-  Future<void> signup(String email, String password) async{
+  _authenticate(String email, String password, String urlSegment) async {
     try {
-      const url =
-          "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAAisBCWejKaemIPKk1AGZca-PaB3pxhZw";
+      final url =
+          "https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=$_appKey";
       final response = await http.post(
         Uri.parse(url),
         body: json.encode(
-          {"email": email, "password": password},
+          {
+            "email": email,
+            "password": password,
+            "returnSecureToken": true,
+          },
         ),
       );
       print(json.decode(response.body));
-      return;
     } catch (e) {
       throw e;
     }
+  }
+
+  Future<void> signup(String email, String password) async {
+    return _authenticate(email, password, "signUp");
+  }
+
+  Future<void> login(String email, String password) async {
+    return _authenticate(email, password, "signInWithPassword");
   }
 }
