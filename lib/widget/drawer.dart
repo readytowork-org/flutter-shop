6f632853f831ec1c7933e869645a8d3c65d3f8d3
bantawa04 <rahunn3@gmail.com>
2022-07-27 15:47:25 +0545

    12.11 Custom animation transistion for individual route

--- lib/widget/drawer.dart
+++ lib/widget/drawer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import '../helpers/custom_route.dart';
 import '../providers/auth.dart';
 import '../screens/user_product_screen.dart';
 import '../screens/orders_screen.dart';
@@ -41,7 +42,12 @@ class SideDrawer extends StatelessWidget {
           }),
           const Divider(),
           _buildDrawer("Orders", Icons.payment, () {
-            Navigator.of(context).pushReplacementNamed(OrdersScreen.routeName);
+            // Navigator.of(context).pushReplacementNamed(OrdersScreen.routeName);
+            Navigator.of(context).pushReplacement(
+              CustomRoute(
+                builder: (ctx) => const OrdersScreen(),
+              ),
+            );
           }),
           const Divider(),
           _buildDrawer("Manage Products", Icons.edit, () {
