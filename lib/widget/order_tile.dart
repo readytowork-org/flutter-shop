41bc435e2a03a8e01fc3116fa5cba64858a1a3d7
bantawa04 <rahunn3@gmail.com>
2022-07-27 15:20:25 +0545

    12.9 Add animation in order tiles

--- lib/widget/order_tile.dart
+++ lib/widget/order_tile.dart
@@ -12,60 +12,69 @@ class OrderTile extends StatefulWidget {
   State<OrderTile> createState() => _OrderTileState();
 }
 
-class _OrderTileState extends State<OrderTile> {
+class _OrderTileState extends State<OrderTile>
+    with SingleTickerProviderStateMixin {
   late bool _expanded = false;
+
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.symmetric(
-        vertical: 8,
-        horizontal: 16,
-      ),
-      child: Column(
-        children: <Widget>[
-          ListTile(
-            title: Text('\$${widget.order.amount}'),
-            subtitle: Text(
-              DateFormat('dd/MM/yyyy hh:mm a')
-                  .format(widget.order.date as DateTime),
-            ),
-            trailing: IconButton(
-              onPressed: () {
-                setState(() {
-                  _expanded = !_expanded;
-                });
-              },
-              icon: Icon(
-                _expanded ? Icons.expand_less : Icons.expand_more,
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeIn,
+      height: _expanded ? min(widget.order.products.length * 20.0 + 110, 200) : 95,
+      child: Card(
+        margin: const EdgeInsets.symmetric(
+          vertical: 8,
+          horizontal: 16,
+        ),
+        child: Column(
+          children: <Widget>[
+            ListTile(
+              title: Text('\$${widget.order.amount}'),
+              subtitle: Text(
+                DateFormat('dd/MM/yyyy hh:mm a')
+                    .format(widget.order.date as DateTime),
               ),
-            ),
-          ),
-          if (_expanded)
-            Container(
-              padding: const EdgeInsets.symmetric(
-                vertical: 8,
-                horizontal: 16,
-              ),
-              height: min(widget.order.products.length * 20.0 + 10, 180),
-              child: ListView.builder(
-                itemBuilder: (context, index) => Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text(
-                      '${widget.order.products[index].title}',
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text(
-                      '\$${widget.order.products[index].price} x ${widget.order.products[index].quantity}',
-                    ),
-                  ],
+              trailing: IconButton(
+                onPressed: () {
+                  setState(() {
+                    _expanded = !_expanded;
+                  });
+                },
+                icon: Icon(
+                  _expanded ? Icons.expand_less : Icons.expand_more,
                 ),
-                itemCount: widget.order.products.length,
               ),
-            )
-        ],
+            ),
+
+              AnimatedContainer(
+                duration: const Duration(milliseconds: 300),
+                curve: Curves.easeIn,
+                padding: const EdgeInsets.symmetric(
+                  vertical: 8,
+                  horizontal: 16,
+                ),
+                height: _expanded ? min(widget.order.products.length * 20.0 + 10, 100) : 0,
+                child: ListView.builder(
+                  itemBuilder: (context, index) => Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        '${widget.order.products[index].title}',
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        '\$${widget.order.products[index].price} x ${widget.order.products[index].quantity}',
+                      ),
+                    ],
+                  ),
+                  itemCount: widget.order.products.length,
+                ),
+              )
+          ],
+        ),
       ),
     );
   }
